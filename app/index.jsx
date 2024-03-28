d86f1e8e5253805e0c3b8f9b3b71a736d643ad30
Nadeeshan Devinda Karunarathna <nadeeshanenterprises20@gmail.com>
2024-03-28 10:56:38 +0530

    Added Drawer Navigation

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+
+
+export default function App() {
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+    <View style={styles.container}>
+      <LoginScreen />
+      
+
+    </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  
+});
