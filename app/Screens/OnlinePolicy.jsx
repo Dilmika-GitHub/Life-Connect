e31d16c86eb480fdcaf2c2f06f0c38302b3c7e14
Nadeeshan Devinda Karunarathna <nadeeshanenterprises20@gmail.com>
2024-03-29 15:48:24 +0530

    Added more pages to the drawer navigation

---
+++ app/Screens/OnlinePolicy.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OnlinePolicy = () => {
+  return (
+    <View>
+      <Text>OnlinePolicy</Text>
+    </View>
+  )
+}
+
+export default OnlinePolicy
