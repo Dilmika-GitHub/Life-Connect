d065c11043e8d26f02330e0be518c0fd9c6d1487
Nadeeshan Devinda Karunarathna <nadeeshanenterprises20@gmail.com>
2024-04-25 16:31:12 +0530

    Started the user profile

--- app/Screens/UserProfile/Profile.jsx
+++ app/Screens/UserProfile/Profile.jsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Your Profile</Text>
-    </View>
-  )
+    <View style={styles.container}>
+    {/* Left half */}
+    <View style={[styles.half, styles.leftHalf]}></View>
+
+    {/* Right half */}
+    <View style={[styles.half, styles.rightHalf]}></View>
+  </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column', // To arrange the two halves horizontally
+  },
+  half: {
+    flex: 1, // Take up half of the screen
+  },
+  leftHalf: {
+    backgroundColor: 'blue', // Set left half color
+  },
+  rightHalf: {
+    backgroundColor: 'green', // Set right half color
+  },
+});
 
 export default Profile
