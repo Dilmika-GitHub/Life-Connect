4797c23636d56db18a8a148bc2980be1dc8af049
Dilmika-GitHub <dilmikawanigarathna@gmail.com>
2024-05-10 11:24:39 +0530

    Maturity drawer update

--- app/Screens/Maturity.jsx
+++ app/Screens/Maturity.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const OnlinePolicy = () => {
+const Maturity = () => {
   return (
     <View>
-      <Text>OnlinePolicy</Text>
+      <Text>Maturity</Text>
     </View>
   )
 }
 
-export default OnlinePolicy
+export default Maturity
