792f0351295b6f44f16d5f11a1ddc9accd4e86ac
Nadeeshan Devinda Karunarathna <nadeeshanenterprises20@gmail.com>
2024-07-25 16:20:22 +0530

    income js updated with the mainmeter code. main meter page deleted. dashboard screen page cleaned

--- app/Screens/DashboardScreen/DashboardScreen.jsx
+++ app/Screens/DashboardScreen/DashboardScreen.jsx
@@ -1,8 +1,6 @@
 import { StyleSheet, Text, View, Animated, ScrollView, Dimensions, BackHandler, Alert } from "react-native";
 import React, { useState, useEffect, useRef } from 'react';
-import Svg, { G, Circle } from "react-native-svg";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { lockToPortrait, lockToAllOrientations } from "../OrientationLock";
 import { useIsFocused } from '@react-navigation/native';
 import Income from "./KPIs/Income";
@@ -12,22 +10,13 @@ import FYPkpi from "./KPIs/FYPkpi";
 import GWPkpi from "./KPIs/GWPkpi";
 import MCFPkpi from "./KPIs/MCFPkpi";
 import CheckConnection from "../../../components/checkConnection";
-import Mainmeter from "./Mainmeter";
 
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-const { height, width } = Dimensions.get('window');
+
+//const { height, width } = Dimensions.get('window');
 
 export default function DashboardScreen({
-  percentage = 65,
-  color = "grey",
-  animatedCircleColor = "#05beda",
-  strokeWidth = wp('8%'),
-  radius = wp('30%'),
   textColor = "black",
-  max = 100,
-  totalvalue = "6,60,00,000",
-  currencyType = "Rs",
 }) {
   const isFocused = useIsFocused();
 
@@ -58,14 +47,7 @@ export default function DashboardScreen({
     return () => backHandler.remove();
   }, []);
 
-  const CircleRef = useRef();
-  const halfCircle = radius + strokeWidth;
-  const viewBoxValue = `0 0 ${halfCircle * 2} ${halfCircle * 2}`;
-  const circleCircumference = 2 * Math.PI * radius;
-  const maxPerc = (100 * percentage) / max;
-  const strokeDashoffset =
-    circleCircumference - (circleCircumference * maxPerc) / 100;
-  const totalvalueText = `${currencyType} ${totalvalue}`;
+  
   React.useEffect(() => {});
 
   return (
@@ -79,9 +61,6 @@ export default function DashboardScreen({
       <View style={styles.iconView}>
         <Income/>
       </View>
-      {/* main meter */}
-      <Mainmeter/>
-      {/* main meter */}
       <View style={styles.kpiContainer}>
         <View style={styles.kpiWrapper}>
           <FPkpi />
