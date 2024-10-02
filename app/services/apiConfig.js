bd68e9d43ec259ffc2d7520e575dd23ef5fa472b
Dilmika-GitHub <dilmikawanigarathna@gmail.com>
2024-10-02 11:57:05 +0530

    add new base url

--- app/services/apiConfig.js
+++ app/services/apiConfig.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://apisvc.srilankainsurance.com/SalesTrackAppAPI/api/v1"; //live
+const BASE_URL = "https://apisvc.sliclife.com/SalesTrackAppAPI/api/v1"; //live
 //const BASE_URL = "http://122.255.4.187:10155/SalesTrackAppAPI/api/v1"; //test
 
 
